a77cadd0c06df44c379e02a24d79f63188b8f81a
matheus <matheusmw21@gmail.com>
2023-10-03 12:19:51 -0300

    ordenando artistas

---
+++ ScreenSoundAPI/Filters/LinqOrder.cs
@@ -0,0 +1,19 @@
+﻿using ScreenSoundAPI.Model;
+
+namespace ScreenSoundAPI.Filters;
+
+internal class LinqOrder
+{
+    public static void Artistas(List<Musica> musicas)
+    {
+        var artistas = musicas.OrderBy(musica => musica.Artista)
+            .Select(musica => musica.Artista).Distinct().ToList() ;
+
+        Console.WriteLine("Lista de Artistas Ordenados");
+
+        foreach (var artista in artistas)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+    }
+}
--- ScreenSoundAPI/Program.cs
+++ ScreenSoundAPI/Program.cs
@@ -9,7 +9,9 @@ using (HttpClient client = new HttpClient())
         string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         var musicas = JsonSerializer.Deserialize<List<Musica>>(response)!;
 
-        LinqFilter.FiltrarGenero(musicas);
+        //LinqFilter.FiltrarGenero(musicas);
+        LinqOrder.Artistas(musicas);
+
     }
     catch (Exception ex)
     {
